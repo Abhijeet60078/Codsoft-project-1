861053da87ba7b16fe3b1270603d8c52be64ee47
Abhijeet Mishra <158300568+Abhijeet60078@users.noreply.github.com>
2024-08-30 00:23:50 +0530

    Calculator.js

---
+++ Calculator.js
@@ -0,0 +1,29 @@
+let display = document.getElementById('display');
+
+function clearDisplay() {
+    display.textContent = '0';
+}
+
+function deleteLast() {
+    if (display.textContent.length > 1) {
+        display.textContent = display.textContent.slice(0, -1);
+    } else {
+        display.textContent = '0';
+    }
+}
+
+function appendToDisplay(value) {
+    if (display.textContent === '0' && value !== '.') {
+        display.textContent = value;
+    } else {
+        display.textContent += value;
+    }
+}
+
+function calculateResult() {
+    try {
+        display.textContent = eval(display.textContent);
+    } catch {
+        display.textContent = 'Error';
+    }
+}
